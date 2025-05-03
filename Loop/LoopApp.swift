3fe1ec1ed3a4efb27755301e6e2c2582ee9a44b6
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-05-03 12:49:30 +1000

    Initial commit

---
+++ Loop/LoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopApp.swift
+//  Loop
+//
+//  Created by Armand Packham-McGuiness on 3/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct LoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
